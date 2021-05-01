2554f96178de580c5ec5f2adf58df3e01b2a84cb
Joy LoRuth <joyloruth@gmail.com>
2021-05-01 11:33:40 -0400

    RandomArray

--- leetcode/RandomArray.java
+++ leetcode/RandomArray.java
@@ -1,9 +1,14 @@
 package leetcode;
 
 public class RandomArray {
+	
+	int[] arr = new int[50];
+	public static int n = 10; 
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
