6711659bca9e1bb413f2d4aa951681018a84e2ab
Joy LoRuth <joyloruth@gmail.com>
2021-05-26 09:29:32 -0500

    PlusMinus hackerRank

--- hackerRank/Algorithms/PlusMinus.java
+++ hackerRank/Algorithms/PlusMinus.java
@@ -1,19 +1,21 @@
 package hackerRank.Algorithms;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class PlusMinus {
-
+	
+	static Scanner scan = new Scanner(System.in);
+	static float denom;
+	
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
 		
-		int denom = scan.nextInt();
-		int neg = 0;
-		int pos = 0;
-		int zero = 0;
+		denom = scan.nextInt();
+		
+		
 		
 		List<Integer> arr = new ArrayList<Integer>();
 		
@@ -22,29 +24,44 @@ public class PlusMinus {
 			arr.add(scan.nextInt());
 		}
 		
-		/*for(int i = 0; i < arr.size(); i++)
-		{
-			if(i > 0) {	++ pos;	}
 			
-			if(i == 0) {++zero;}
 			
-			else { ++neg;}
-		}*/
-			//System.out.println("neg: " + neg  + " pos: " + pos + " zero: " + zero);
-
 			
-			for(int num: arr) {
-				System.out.println(num);
-			}
+		PlusMinus minus = new PlusMinus();
 		
-		//PlusMinus minus = new PlusMinus();
-		//minus.plusMinus(arr);
 		
 		
 
 	}
 	
-	List<Integer> plusMinus(List<Integer> arr){
+	List<Float> plusMinus(List<Float> arr){
+		
+		float neg = 0f;
+		float pos = 0f;
+		float zero = 0f;
+
+		for(float num: arr)
+		{
+			if(num > 0) {	++ pos;	}
+			
+			if(num == 0) {++zero;}
+			
+			if(num < 0) { ++neg;}
+		}
+			
+		
+			float neg_occurence = neg / denom;
+			float pos_occurence = pos/denom;
+			float zero_occurence = zero/denom;
+			
+			
+			DecimalFormat df = new DecimalFormat("0.000000");
+			System.out.println(df.format(neg_occurence));
+			System.out.println(df.format(pos_occurence));
+			System.out.println(df.format(zero_occurence));
+			
+			
+			
 		return arr;
 	}
 
