604da2ec1f16416edb9c4f6c9eb283b8acc25c88
Joy LoRuth <joyloruth@gmail.com>
2021-06-02 10:45:20 -0500

    time conversion challenge

--- hackerRank/Algorithms/TimeConversion.java
+++ hackerRank/Algorithms/TimeConversion.java
@@ -1,10 +1,20 @@
 package hackerRank.Algorithms;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeConversion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DateFormat dateformat = new SimpleDateFormat();
+		String date = "1:00 PM";
+	//	date.format(date).
 
 	}
-
+	
+	String timeConservation(String time) {
+		return time;
+	}
 }
