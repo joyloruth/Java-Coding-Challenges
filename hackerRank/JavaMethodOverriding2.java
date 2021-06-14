4e2586f827f375708306d176a274d3af1560cb63
Joy LoRuth <joyloruth@gmail.com>
2021-06-13 22:46:48 -0500

    java coding practice - method override

--- hackerRank/JavaMethodOverriding2.java
+++ hackerRank/JavaMethodOverriding2.java
@@ -6,16 +6,40 @@ import java.util.regex.Pattern;
 public class JavaMethodOverriding2 {
 
 	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	
+	
 		
-	//Pattern pat = new Pattern(scan.next(), scan.nextInt());
+		
+	public static void main(String[] args)
+	{
 		
 	}
 	
-	public static Pattern compile(String regex)
+		
+	class Bicycle
 	{
-		return null;
+		String define_me()
+		{
+			return "a vechicle with pedals";
+		}	
 	}
+		
+	public class Motorcycle extends Bicycle{
+		
+		String define_me()
+		{
+			return "a cycle with an engine";
+		}
+		
+		Motorcycle()
+		{
+			String temp = super.define_me();
+			System.out.println("motor sport");
+		}
+		
+		
+	}
+	
+	
 
 }
