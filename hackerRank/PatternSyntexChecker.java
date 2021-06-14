ba5e47ee440d3e2aeb7f8261fc92b7851a357978
Joy LoRuth <joyloruth@gmail.com>
2021-06-13 22:23:18 -0500

    PatternSyntex Java algo practice

---
+++ hackerRank/PatternSyntexChecker.java
@@ -0,0 +1,40 @@
+package hackerRank;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntexChecker {
+
+	public static void main(String[] args)
+	
+	{
+		
+		// ORACLE DOCUMENT 
+		// https://docs.oracle.com/javase/6/docs/api/java/util/regex/Pattern.html#compile%28java.lang.String%29
+		
+		//For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
+		
+		Scanner scan = new Scanner(System.in);
+		int testCases = scan.nextInt();
+		
+		for(int i = 0; i < testCases; i++) {
+			
+			String pattern = scan.next();
+			
+			try
+			{
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			}
+			
+			catch(PatternSyntaxException e) 
+			{
+				System.out.println("Invalid");
+			}
+			
+		}
+		
+	}
+
+}
