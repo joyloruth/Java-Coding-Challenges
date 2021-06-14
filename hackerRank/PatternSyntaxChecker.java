f2a9d75a2895e93331f40bfbc436e7d1e4125ec4
Joy LoRuth <joyloruth@gmail.com>
2021-06-13 22:28:28 -0500

    coding practice, pattern syntax

--- hackerRank/PatternSyntaxChecker.java
+++ hackerRank/PatternSyntaxChecker.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-public class PatternSyntexChecker {
+public class PatternSyntaxChecker {
 
 	public static void main(String[] args)
 	
