0f26c6a180fcc57155c4eb10f345ab31e0ff0b53
Joy LoRuth <joyloruth@gmail.com>
2021-06-16 22:26:33 -0500

    CodeWars coding cahallenge to reverse the order of words in a sentence

---
+++ codeWars/ReverseASentence.java
@@ -0,0 +1,67 @@
+package codeWars;
+
+import java.util.Scanner;
+
+public class ReverseASentence {
+
+	public static void main(String[] args) {
+		
+	/*	String word = "I love myself";
+		
+		String[] sentence =  word.split(" ");
+		
+		StringBuilder builder = new StringBuilder();
+		
+		
+		for(int i = sentence.length -1; i >= 0; i--) {
+			builder.append(sentence[i]).append(" ");
+		}
+		
+		System.out.println(builder);*/
+		
+		ReverseASentence sent = new ReverseASentence();
+		String str = "I love myself";
+		
+		sent.reverseWords(str);
+		
+	}
+	
+	public static String reverseWords(String str) {
+		
+	   
+		
+		String[] sentence =  str.split(" ");
+		
+		 String word = "";
+		
+		for(int i = sentence.length -1; i >= 0; i--) {
+			word += sentence[i] + " ";
+		}
+		System.out.println(word);
+		return word;
+		
+		
+		
+	}
+	
+	public static StringBuilder reverseTwo(String str) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		String[] word = input.split(" ");
+		StringBuilder builder = new StringBuilder();
+		String revWord = "";
+		
+		for(int i = word.length - 1; i >= 0; i--)
+		{
+			builder.append(word[i]).append(" ");
+		}
+		
+		
+		System.out.println(builder);
+		return builder;
+		
+	}
+
+}
