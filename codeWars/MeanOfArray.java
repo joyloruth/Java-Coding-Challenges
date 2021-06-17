4cf6470859840c41e44956424959d67d3f695881
Joy LoRuth <joyloruth@gmail.com>
2021-06-17 02:31:59 -0500

    codewars - find the mean of an int array

---
+++ codeWars/MeanOfArray.java
@@ -0,0 +1,33 @@
+package codeWars;
+
+public class MeanOfArray {
+
+	public static void main(String[] args) {
+		
+	MeanOfArray mean = new MeanOfArray();
+	
+		int[] marks = {2,2,2,2,2};
+		
+		System.out.println(mean.getAverage(marks));
+
+	}
+	
+	public int getAverage(int[] marks) {
+		
+		
+		int arrsize = marks.length;
+		int total = 0;
+		
+		for(int i = 0; i < marks.length; i++) {
+			total += marks[i];
+			
+		}
+		int mean = total/arrsize;
+		
+		
+		return mean;
+	}
+	
+	
+
+}
