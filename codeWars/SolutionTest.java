2908565d5d0d18f51cca04ac8749d7e9a8b2f22e
Joy LoRuth <joyloruth@gmail.com>
2021-06-17 23:46:17 -0500

    TestCases

---
+++ codeWars/SolutionTest.java
@@ -0,0 +1,45 @@
+package codeWars;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+	
+	
+	
+	@Test
+    public void testFixed() {
+      //  assertEquals("S.H", AbbreviateTwoWords.abbrevName("Sam Harris"));
+      //  assertEquals("P.F", AbbreviateTwoWords.abbrevName("Patrick Feenan"));
+      //  assertEquals("E.C", AbbreviateTwoWords.abbrevName("Evan Cole"));
+       // assertEquals("P.F", AbbreviateTwoWords.abbrevName("P Favuzzi"));
+       // assertEquals("D.M", AbbreviateTwoWords.abbrevName("David Mendieta"));
+    }
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAbbreviateTwoWords() {
+		fail("Not yet implemented");
+	}
+
+}
