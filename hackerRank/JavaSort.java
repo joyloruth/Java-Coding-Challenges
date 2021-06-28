f44cf5f3c0df006f147ba1944e34f65304229826
Joy LoRuth <joyloruth@gmail.com>
2021-06-28 02:32:54 -0500

    java sort

---
+++ hackerRank/JavaSort.java
@@ -0,0 +1,26 @@
+package hackerRank;
+
+import java.util.HashMap;
+
+public class JavaSort {
+
+	public static void main(String[] args) {
+	
+	int num = 5; 
+	int[] arr = new int[num + 1];
+	
+	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+	
+	
+	for(int i = 1; i <= num; i++) {
+		map.put(i, i);
+		
+	}
+	
+	int[] A = {0,1,2,3,5};
+		
+		System.out.println(map);
+		
+	}
+
+}
