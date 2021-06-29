9dd6657b96dbe4fe306ed4264ad7f2a7922ddc4b
Joy LoRuth <joyloruth@gmail.com>
2021-06-29 13:24:52 -0500

    completed HackerRank Hashset coding challenge

---
+++ hackerRank/Hashset.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package hackerRank;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @author Joy LoRuth
+ *
+ */
+public class Hashset {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scan  = new Scanner(System.in);
+		int t = 5;
+		HashSet<String> set = new HashSet<String>();
+		
+		String[] right = {"john","john", "john", "mary", "mary" };
+		String[] left = {"tom","mary", "tom", "anna", "anna"};
+		
+		
+		
+		for(int i = 0; i < t ; i++) {
+			set.add(right[i] + " "+ left[i] );
+			System.out.println(set.size());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		
+	
+
+}
+}
