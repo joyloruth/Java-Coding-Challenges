ae3b98c8e74e39bbf33e4fe2d3151ade3ca809dc
Joy LoRuth <joyloruth@gmail.com>
2021-07-15 01:11:09 -0500

    update

--- codeWars/ComplementaryDNA.java
+++ codeWars/ComplementaryDNA.java
@@ -6,23 +6,8 @@ public class ComplementaryDNA {
 
 	public static void main(String[] args) {
 		String dna = "TATCCGG";
-		HashMap<String,String> map = new HashMap<String,String>();
-		map.put("A", "T");
-		map.put("T", "A");
-		map.put("C", "G");
-		map.put("G", "C");
-		for(int i = 0 ; i < dna.length(); i++) {
-			
-		}
-		
-	
 		
 		
-		
-		System.out.println(makeComplement(dna));
-		
-		 
-
 	}
 	
 	 public static String makeComplement(String dna) {
