7dcc043da3209043953309900239082159362daa
Joy LoRuth <joyloruth@gmail.com>
2021-08-01 00:05:51 -0500

    update

--- hackerRank/dataStructures/SubArray.java
+++ hackerRank/dataStructures/SubArray.java
@@ -3,16 +3,18 @@ package hackerRank.dataStructures;
 public class SubArray {
 
 	public static void main(String[] args) {
-		int[] a = {1,-2,4,5,1};
-		int sum = 0;
+		int[] arr = {1,-2,4,5,1};
 		
-		for(int i = 0; i < a.length; i++) {
-			for(int j = i+1; j < a.length; j++) 
+		int sum = 0 ; 
+		
+		for(int i = 0; i < arr.length; i++) 
+		{
+			
+			for(int j = i; j < arr.length; j++) 
 			{
-				sum = a[i] + a[j];
-				System.out.println(a[i] +" "+ a[j] +  " = " + sum);
-				
+				sum = sum + arr[j];
 			}
+				
 		}
 
 	}
